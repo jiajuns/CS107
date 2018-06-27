477959b2c11ea9d81f170a127f3109f92a0fafc2
jiajuns <jiajunstanley@gmail.com>
2018-06-26 22:54:32 -0700

    question 2 finished

--- assign2/util.c
+++ assign2/util.c
@@ -1,6 +1,6 @@
 #include "samples/prototypes.h"
 #include <string.h>
-
+#include <stdio.h>
 
 const char *get_env_value(const char *envp[], const char *key)
 {
@@ -18,25 +18,41 @@ const char *get_env_value(const char *envp[], const char *key)
 }
 
 
+// bool scan_token(const char **p_input, const char *delimiters, char buf[], size_t buflen)
+// {   
+//     if (!**p_input) return false;
+//     // use strspn find the index 'start' of first non-delim/valid char, matched with delim the program will keep searching
+//     int start = strspn(*p_input, delimiters);
+//     // move p_input to fist valid char
+//     *p_input += start;
+//     // if no valid char, return false
+//     if (!**p_input) return false;
+//     // use strcspn find the index 'end' of the next delim
+//     int end = strcspn(*p_input, delimiters);
+//     // determine the num of chars to write in buf
+//     int n = (end > buflen - 1) ? (buflen - 1) : end;
+//     // for n loop, write one char at **p_input, move *p_input forward by 1
+//     for (int i = 0; i < n; i++) {
+//         buf[i] = **p_input;
+//         *p_input += 1;
+//     }
+//     // add null terminator at the end of buffer
+//     buf[n] = '\0'; 
+//     return true;
+// }
+
 bool scan_token(const char **p_input, const char *delimiters, char buf[], size_t buflen)
-{   
+{
     if (!**p_input) return false;
-    // use strspn find the index 'start' of first non-delim/valid char
     int start = strspn(*p_input, delimiters);
-    // move p_input to fist valid char
     *p_input += start;
-    // if no valid char, return false
     if (!**p_input) return false;
-    // use strcspn find the index 'end' of the next delim
     int end = strcspn(*p_input, delimiters);
-    // determine the num of chars to write in buf
-    int n = (end > buflen - 1) ? (buflen - 1) : end;
-    // for n loop, write one char at **p_input, move *p_input forward by 1
-    for (int i = 0; i < n; i++) {
+    int n = (end > buflen - 1) ? (buflen - 1): end;
+    for (int i = 0; i < n; i++){
         buf[i] = **p_input;
         *p_input += 1;
     }
-    // add null terminator at the end of buffer
-    buf[n] = '\0'; 
+    buf[n] = '\0';
     return true;
 }

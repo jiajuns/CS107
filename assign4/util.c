9e99a335852fff7b9052b667a7b173df9e89489d
jiajuns <jiajunstanley@gmail.com>
2018-07-29 22:55:15 -0700

    finish question 4 hope it dose not have bug

--- assign4/util.c
+++ assign4/util.c
@@ -1,37 +1,39 @@
 #include "samples/prototypes.h"
 #include <string.h>
 
-void *binsert(const void *key, void *base, size_t *p_nelem, size_t width, int (*compar)(const void *, const void *))
-{
-    if (*p_nelem == 0){
-        *p_nelem += 1;
-        return memmove(base, key, width);
-    }
-    void *found = NULL;
-    void *start = base;
-    for (size_t nremain = *p_nelem; nremain != 0; nremain >>= 1) {
-        void *p = (char *)base + (nremain >> 1) * width;
-        int sign = compar(key, p);
-        if (sign == 0) {
-            found = p;
-            break;
-        }
-        if (sign > 0) {
-            base = (char *)p + width;
-            nremain--;
-        }
+
+void *binsert(const void *key, void *base, size_t *nel, size_t width, int (*cmp)(const void *, const void *))
+{   
+    if (*nel == 0)
+    {
+        *nel += 1;
+        memmove(base, key, width);
+        return base;
     }
 
-    if (found) {
+    void *start = base;
+    void *found = NULL;
+    for (size_t nremain = *nel; nremain != 0; nremain >>= 1) {
+        void *p = (char *)base + (nremain >> 1) * width;
+        int sign = cmp(key, p);
+        if (sign == 0)
+            found = p;
+            break;
+        if (sign > 0) {  /* key > p: move right */
+            base = (char *)p + width;
+            nremain--;
+        }       /* else move left */
+    }
+
+    if (found)
+    {
         return found;
     } else {
-        void *end = (char *)start + (*p_nelem - 1) * width;
-        while (end >= base) {
-            memmove((char *)end + width, end, width);
-            end = (char *)end - width;
-        }
-        *p_nelem += 1;
-        return memmove(base, key, width);
+        void *end = (char *)base + width;
+        memmove(end, base, width*(*nel-1)-((char *)base - (char *)start));
+        *nel += 1;
+        memmove(base, key, width);
+        return base;
     }
-    
 }
+
